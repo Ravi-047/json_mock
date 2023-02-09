75777c556726a58bc00b1efdb89462cf3b5fd9d6
Ravi-047 <ravi047raj@gmail.com>
2023-02-09 10:07:57 +0530

    mock_server

---
+++ index.js
@@ -0,0 +1,15 @@
+// creating a server.
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+const PORT = process.env.PORT || 8080
+
+
+server.use(middlewares)
+server.use(router)
+
+// listening the server
+server.listen(PORT, () => {
+    console.log('JSON Server is running')
+})
